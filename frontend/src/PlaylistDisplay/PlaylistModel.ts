bb539a16fcbd6c71c6cf32d8d085f2d8a5dc050f
abdullah <a.ismail@mailbox.org>
2022-03-30 17:53:53 +0200

    #5 ready to review

---
+++ frontend/src/PlaylistDisplay/PlaylistModel.ts
@@ -0,0 +1,22 @@
+
+interface PlaylistArtist{
+    name : string
+}
+
+interface PlaylistTrack{
+    title: string,
+    artists : Array<PlaylistArtist>,
+    album: string,
+    albumReleaseDate : string
+}
+
+interface PlaylistImage{
+    url: string
+}
+
+export interface PlaylistsResponse{
+    name : string,
+    tracks : Array<PlaylistTrack>,
+    images : Array<PlaylistImage>
+
+}
--- frontend/src/PlaylistDisplay/PlaylistOverview.tsx
+++ frontend/src/PlaylistDisplay/PlaylistOverview.tsx
@@ -1,15 +1,27 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import PlaylistItem from "./PlaylistItem"
+import { PlaylistsResponse } from "./PlaylistModel"
+
+
+
 
 export default function PlaylistOverview() {
 
+    const [playlists, setPlaylists] = useState([] as Array<PlaylistsResponse>)
 
     useEffect(() => {
         fetch('http://localhost:8080/api/playlists', {
             method: 'GET'
         })
+        .then(request => request.json())
+        .then(requestBody => setPlaylists(requestBody))
     })
 
     return(
-        <div>Hallo World!</div>
+        <div>
+            <h2>Your Playlists</h2>
+            {playlists.
+            map(item => <PlaylistItem name={item.name} images={item.images}/>)}
+        </div>
     )
 }
