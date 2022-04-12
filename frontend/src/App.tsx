da6283d0d353022839791b2c4f58c20aedb14827
abdullah <a.ismail@mailbox.org>
2022-04-12 14:23:05 +0200

    import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
+import Navigation from "./Navigation";
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
         <Suspense fallback="loading..">
         <div>
         <Header />
+        <Navigation />
         <Outlet />
         </div>
         </Suspense>
