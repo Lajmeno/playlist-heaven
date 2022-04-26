12eb8d18d3a267906152eeeb9f5f98467f164d45
abdullah <a.ismail@mailbox.org>
2022-04-26 12:09:53 +0200

    added Info to login-page

--- frontend/src/Login.tsx
+++ frontend/src/Login.tsx
@@ -18,8 +18,9 @@ export default function Login(){
                 />
             </Figure>
             <Container>
+                <Row style ={{"color":"yellow"}}>Info: In the current development state, this app can only be used by individuals who are enlisted by the developer to Spotify. For testing please contact the developer.</Row>
                 <Row>
-                    <a href={`https://accounts.spotify.com/authorize?response_type=code&client_id=80640b8612764947a6329d3103743e02&scope=user-read-private user-read-email playlist-read-private playlist-read-collaborative playlist-modify-public playlist-modify-private&redirect_uri=${process.env.REACT_APP_CALLBACK_URL}`}><Button >Spotify-Login</Button></a>
+                    <a href={`https://accounts.spotify.com/authorize?response_type=code&client_id=80640b8612764947a6329d3103743e02&scope=user-read-private playlist-read-private playlist-read-collaborative playlist-modify-public playlist-modify-private&redirect_uri=${process.env.REACT_APP_CALLBACK_URL}`}><Button >Spotify-Login</Button></a>
                 </Row>
              <Row >
                 <a href="/overview" style={{"display": "none"}}><Button>Home</Button></a>
